3f71c6467fa56150c02afcd35b109ce23f9d1fdb
Ankit Srivastava <52760842+wintersoldier24@users.noreply.github.com>
2026-06-26 02:34:08 +0530

    Time: 1 ms (60.59%), Space: 61.2 MB (58.29%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.js
+++ 0344-reverse-string/0344-reverse-string.js
@@ -6,7 +6,7 @@ var reverseString = function (s) {
   // return s.reverse()
   let i = 0;
   let j = s.length - 1;
-  while (i <= j) {
+  while (i < j) {
     let temp = s[i];
     s[i] = s[j];
     s[j] = temp;
